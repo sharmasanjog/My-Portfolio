3946c0fb0e9418a69cbdcb0172a7eb7158141047
Sanjog Sharma <sharmasanjog2005@gmail.com>
2025-09-28 02:48:20 +0545

    Fix package.json, tsconfig, and imports

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    optimizeDeps: {
-        exclude: ['lucide-react'],
-    },
-});
+    base: '/', // Required for custom domain deployment
+})
