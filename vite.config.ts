09194a05d2c4d19ba2860f69bfe7090e7d4ab50a
Sanjog Sharma <sharmasanjog2005@gmail.com>
2025-09-28 13:52:28 +0545

    Modification

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/', // Required for custom domain deployment
+    base: '/My-Portfolio/', // Required for custom domain deployment
 })
